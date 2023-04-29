6aeed0632c021582d70f50df163f8111253a7d66
asma <a.fellah@esi-sba.dz>
2023-04-29 23:39:12 +0000

    first commit

---
+++ chat.py
@@ -0,0 +1,60 @@
+import random
+import json
+import time
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+#import telegram
+#from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Home"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                if tag == 'datetime':
+                    print(time.strftime("%A"))
+                    print(time.strftime("%D %B %Y"))
+                    print(time.strftime("%H:%M:%S"))
+
+    else:
+        print(f"{bot_name}: I do not understand...")
